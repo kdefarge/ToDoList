c263aa40aa9ffd55af21f4d6820a1610258f76c9
Kévin DEFARGE <1819360+kdefarge@users.noreply.github.com>
2021-05-03 03:43:10 +0200

    Migrate old version to LTS

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage", methods={"GET"})
+     */
+    public function indexAction()
+    {
+        return $this->render('default/index.html.twig');
+    }
+}
