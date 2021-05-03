227ea1a81a6d5b3e2e157711881b8f2347319614
Kévin DEFARGE <1819360+kdefarge@users.noreply.github.com>
2021-05-03 19:38:47 +0200

    Anonymous task author - Returns a user with a username 'Anonyme' - Add isAnonymous method on Task - Add validation to not be able to create an anonymous user - Add a datamapper on TaskType to see the author user name

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -2,23 +2,50 @@
 
 namespace App\Form;
 
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\DataMapperInterface;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormInterface;
 
-class TaskType extends AbstractType
+class TaskType extends AbstractType implements DataMapperInterface
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title')
             ->add('content', TextareaType::class)
-            ->add('author', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'username',
+            ->add('author', TextType::class, [
                 'disabled' => true,
-            ]);
+            ])
+            ->setDataMapper($this)
+        ;
+    }
+
+    public function mapDataToForms($viewData, $forms): void
+    {
+        if (!$viewData instanceof Task) {
+            throw new UnexpectedTypeException($viewData, Color::class);
+        }
+
+        /** @var FormInterface $forms */
+        $forms = iterator_to_array($forms);
+        
+        $forms['title']->setData($viewData->getTitle());
+        $forms['content']->setData($viewData->getContent());
+        $forms['author']->setData($viewData->getAuthor()->getUsername());
+    }
+
+    public function mapFormsToData($forms, &$viewData): void
+    {
+         /** @var FormInterface $forms */
+        $forms = iterator_to_array($forms);
+
+         /** @var Task $viewData */
+        $viewData->setTitle($forms['title']->getData());
+        $viewData->setContent($forms['content']->getData());
     }
 }
