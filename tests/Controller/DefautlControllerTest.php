c947a0f71f403a7e76d94211703c86198c5fdc88
Kevin DEFARGE <1819360+kdefarge@users.noreply.github.com>
2021-05-04 17:39:53 +0200

    Coverage and template fix : - Test Homepage - Test UserList - Test CreateUser - Test EditUser - Template : add "role" col in user list - Update code-coverage

--- tests/Controller/DefautlControllerTest.php
+++ tests/Controller/DefautlControllerTest.php
@@ -3,14 +3,48 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 
 class DefautlControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    use ReloadDatabaseTrait;
+
+    const LINK_CREATEUSER   = 'Créer un utilisateur';
+    const LINK_TASKS        = 'Consulter la liste des tâches à faire';
+
+    public function testHomepageGuest(): void
     {
         $client = static::createClient();
         $client->request('GET', '/');
 
         $this->assertResponseRedirects('http://localhost/login', 302);
+    }
+
+    public function testHomepageUser(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/', [], [], [
+            'PHP_AUTH_USER' => 'user1',
+            'PHP_AUTH_PW'   => 'toctoc',
+        ]);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSame(false, $crawler->selectLink(self::LINK_CREATEUSER)->count() >= 1);
+        $this->assertSame(true, $crawler->selectLink(self::LINK_TASKS)->count() >= 1);
+    }
+
+    public function testHomepageAdmin(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/', [], [], [
+            'PHP_AUTH_USER' => 'admin',
+            'PHP_AUTH_PW'   => 'toctoc',
+        ]);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSame(true, $crawler->selectLink(self::LINK_CREATEUSER)->count() >= 1);
+        $this->assertSame(true, $crawler->selectLink(self::LINK_TASKS)->count() >= 1);
     }
 }
